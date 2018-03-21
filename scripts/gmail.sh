63c8d09e5ad801865d403cbc5b39ee12532da781
Luc Trudeau <luc@trud.ca>
2018-03-20 21:26:55 -0400

    Change Gmail from Firefox to Chrome

--- scripts/gmail.sh
+++ scripts/gmail.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-#Launches firefox in app mode with Gmail
-firefox -P Gmail --new-instance https://mail.google.com/mail/u/1/#inbox
+# Open Google Gmail as an app
+
+# Firefox Quantum broke this so I have to switch to chrome
+#firefox -P Gmail --new-instance https://mail.google.com/mail/u/1/#inbox
+
+chromium --app=https://mail.google.com/mail/u/1/#inbox
