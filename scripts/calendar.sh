29bc0b53f5187769d34324fd4b914c8d250ca8a1
Luc Trudeau <luc@trud.ca>
2018-03-20 21:23:53 -0400

    Change Calendar from Firefox to Chrome

--- scripts/calendar.sh
+++ scripts/calendar.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-# Launches Firefox in app mode with Google Calendar 
-firefox -P Calendar --new-instance https://calendar.google.com/calendar/b/1/render#main_7
+# Open Google Calendar as an app
+
+# Firefox Quantum broke this so I have to switch to chrome
+#firefox -P Calendar --new-instance https://calendar.google.com/calendar/b/1/render#main_7
+
+chromium --app=https://calendar.google.com/
